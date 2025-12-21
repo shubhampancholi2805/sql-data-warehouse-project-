2027f6ce7d79d84096d9b1250c9ff47d624513f7
SHUBHAM PANCHOLI <shubhampancholi28@gmail.com>
2025-12-21 07:58:25 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,14 @@
-
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+Additionally, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+*/
+--creating database 'DataWarehouse'
+create database DataWarehouse
+--creating schemas 'bronze', 'silver', and 'gold'.
+create schema bronze ;
+create schema silver ;
+create schema gold ;
